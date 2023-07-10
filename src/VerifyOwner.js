e6149ad02f64e180e6b7580496641947f185f617
Mihir <mihir6453@gmail.com>
2023-07-10 09:04:05 +0530

    initial react setup

---
+++ src/VerifyOwner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import UploadButton from "./UploadButton";
+
+function VerifyOwner() {
+    return(
+        <div>
+            <Navbar/>
+            <div>Verify Owner Page</div>
+            <UploadButton></UploadButton>
+        </div>
+    )
+}
+
+export default VerifyOwner
