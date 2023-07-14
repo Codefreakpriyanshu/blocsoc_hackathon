e1aebbd8b0c0150fe5fff353812001b2e1617d95
Codefreak <118419195+Codefreakpriyanshu@users.noreply.github.com>
2023-07-14 17:43:12 +0530

    Update app.js

--- client/src/backend/app.js
+++ client/src/backend/app.js
@@ -2,6 +2,7 @@ const express = require(`express`)
 const app = express()
 const mongoose = require(`mongoose`)
 const mongodb = require(`mongodb`)
+const Track_data = require('./formDetails')
 //conectin to database
 const DB = `mongodb+srv://mihir1101:mihir1101@cluster0.w0iczi8.mongodb.net/logindata?retryWrites=true&w=majority`
 mongoose.connect(DB, {
@@ -29,6 +30,7 @@ app.post("/UploadYourMusic", async (req, res) => {
             artistName: req.body.artistName,
             songName: req.body.musicName,
             songUrl: req.body.musicUrl,
+            track_artURL : req.body.imageURL
         })
         res.render('upload')
 
@@ -39,3 +41,15 @@ app.post("/UploadYourMusic", async (req, res) => {
 
 app.listen(3000, () => {
 })
+
+let track_data = Track_data.find({},function(err,tracks){
+    if(err) console.warn(err);
+    return tracks;
+})
+export {track_data};
+song_list_container=document.querySelector('.song-list')
+window.onload() = function Song_list(){
+    for(let i = 0;i<track_data.length;i++){
+        song_list_container.innerHTML = `<img class ="track-art" src = '${track_data[i].track_artURL}'> <span class="track-name"> ${track_data[i].songName} </span> <span class="artist-name"> ${track_data[i].artistName} </span> <img class="download-icon" src = "../../public/download-icon.png"> <button class="btn">play</button> `;
+    }
+}
